626fc011e72b264198622aac0cf28afce2d8595b
Nikola Vlčková <nikiwolffe@seznam.cz>
2025-05-14 22:12:56 +0200

    meow

---
+++ main.js
@@ -0,0 +1,50 @@
+let dvd = document.getElementById('dvd')
+let x_inc = 4;
+let y_inc = 4;
+let colour = 0;
+
+function init() {
+
+    dvd.style.position = 'absolute';
+    setInterval(frame, 5);
+
+}
+
+function update_colour(){
+    if (colour == 360){
+        colour = 0;
+    }
+    dvd.style.filter = `hue-rotate(${colour}deg)`;
+    colour++;
+    colour++;
+}
+
+function handle_collision() {
+
+    let dvd_height = dvd.offsetHeight;
+    let dvd_width = dvd.offsetWidth
+    let dvd_top = dvd.offsetTop
+    let dvd_left = dvd.offsetLeft
+    let win_height = window.innerHeight;
+    let win_width = window.innerWidth;
+
+    if (dvd_left <= 0 || dvd_left + dvd_width >= win_width) {
+        x_inc = ~x_inc + 1
+    }
+
+    if (dvd_top <= 0 || dvd_top + dvd_height >= win_height) {
+        y_inc = ~y_inc + 1
+    }
+
+
+}
+
+function frame() {
+    handle_collision();
+    update_colour();
+
+    dvd.style.top = dvd.offsetTop + y_inc;
+    dvd.style.left = dvd.offsetLeft + x_inc;
+}
+
+init();
